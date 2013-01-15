0b7a57526540646e787c520bca7d74551c0a03f2
bugimetal <bugimetal@gmail.com>
2013-01-15 10:16:15 +0200

    view update

--- app/banner/views.py
+++ app/banner/views.py
@@ -11,7 +11,7 @@ from app.banner.models import Banner
 def index(request):
     current_day = time.strftime('%Y-%m-%d')
     current_hour = int(time.strftime('%H'))
-    available_banners = list(Banner.objects.filter(is_enabled='1', start_date__gte=current_day,
+    available_banners = list(Banner.objects.filter(is_enabled=True, start_date__lte=current_day,
                                                    end_date__gte=current_day,
                                                    dayparting__hour=current_hour).values())
 
